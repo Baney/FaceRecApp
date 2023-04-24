61b7c4658cb8eb5bacf1e6578e36beefe6b07f01
Baney <peterbaney@gmail.com>
2023-04-24 10:06:22 +0100

    Initial Commit

---
+++ ImageGrabber/ImageGrabber/Http/HttpStuff.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading;
+using System.Dynamic;
+using System.Net.Http.Headers;
+
+namespace ImageGrabber.Http
+{
+    internal class HttpStuff
+    {
+        readonly string _endPoint;
+        readonly string _ipAddress;
+        public HttpStuff(string Endpoint, String IpAddr)
+        {
+            _endPoint = Endpoint;
+            _ipAddress = IpAddr;
+        }
+
+        public async Task<string> Grabit()
+        {
+            try
+            {
+                using var httpClient = new HttpClient();
+
+                var credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes("admin:1234abcd"));
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+                
+                var response = await httpClient.GetAsync(BuildEndpointRequest());
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+                throw;
+            }
+
+        }
+
+        private string BuildEndpointRequest()
+        {
+            
+            var endpointRequest = "ISAPI/System/status";
+            var requestUrl = ($"http://{_ipAddress}/{endpointRequest}");
+
+            return requestUrl;
+        }
+    }
+
+    public enum EndPointUrls
+    {
+        GetModel,
+        TrigerRelay,
+        GrabImage,         
+    }  
+}
---
+++ ImageGrabber/ImageGrabber/Program.cs
@@ -0,0 +1,26 @@
+﻿using ImageGrabber.Http;
+using System.Net;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Camera IP address: ");
+        var ipAddr = Console.ReadLine();
+
+        Console.WriteLine("Pick an Option\n" +
+                          "1.Get Cameras Model\n" +
+                          "2.Trigger Cameras Relay\n" +
+                          "3.Retreive an image");
+        var endPointrequest = Console.ReadLine();
+
+        var getit = new HttpStuff(endPointrequest,ipAddr);
+
+        Task<String> task =  getit.Grabit();
+
+        string result = task.Result;
+
+        Console.WriteLine($"{task.Result}");
+        Console.ReadLine(); 
+    }
+}
